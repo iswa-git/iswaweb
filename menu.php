7c056500f6105541ba95f6fe465d6d951f31b151
mebreek <github@ibrik.net>
2017-11-29 23:20:15 +0200

    added menu file and changed filenames (hr and index) to go with the menu

---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+    <div class="topnav" id="myTopnav">
+            <a href="index.php" class="active">Home</a>
+            <a href="hr.php">HR</a>
+            <a href="#stock">Stock</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </div> 
+    ';
